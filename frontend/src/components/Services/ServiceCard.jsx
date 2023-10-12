f09baa3826e3f93980922aef986bd458cbd45333
MirandaMG20 <guadamiran@gmail.com>
2023-10-12 14:34:29 -0400

    Home

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -17,13 +17,17 @@ const ServiceCard = ({ item, index }) => {
             <div className="flex items-center justify-between mt-[30px]">
                 <Link
                     to="/doctors"
-                    className="w-[44px] h-[44px] rounded-full border border-solid border-[#181a1e] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                    className="w-[44px] h-[44px] rounded-full border border-solid border-[#181a1e] flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
                     <BsArrowRight className="group-hover:text-white w-6 h-5" />
                 </Link>
                 <span
                     className="w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]"
-                    style={{ background: `${bgColor}`, color: `${textColor}`, borderRadius: '6px 0' }}>
+                    style={{
+                        background: `${bgColor}`,
+                        color: `${textColor}`,
+                        borderRadius: '6px 0'
+                    }}>
                     {index + 1}
                 </span>
             </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-// import React from 'react'
+import React from 'react'
 import heroImg01 from '../assets/images/hero-img01.png'
 import heroImg02 from '../assets/images/hero-img02.png'
 import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
+import featureImg from '../assets/images/feature-img.png'
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
 import About from '../components/About/About'
@@ -167,6 +168,56 @@ const Home = () => {
       </section>
       {/* SERVICES SECTION END */}
 
+      {/* FEATURE SECTION */}
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/* FEATURE CONTENT */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get virtual treatment <br /> anytime.
+              </h2>
+              <ul className="pl-4">
+                <li className="text__para">
+                  1. Schedule the appointment directly.
+                </li>
+                <li className="text__para">
+                  2. Search for your physician here, and contact their office.
+                </li>
+                <li className="text__para">
+                  3. View our physician who are accepting new patients, use the online scheduling tool to select an appointment time.
+                </li>
+              </ul>
+              <Link to='/'>
+                <button className="btn">
+                  Learn More
+                </button>
+              </Link>
+            </div>
+            {/* FEATURE IMG */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} className='w-3/4' alt='' />
+
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tues, 24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
+                      10:00AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px[9px]">
+                    <img src={videoIcon} alt='' />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* FEATURE SECTION END*/}
     </>
   )
 }
