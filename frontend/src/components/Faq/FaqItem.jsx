91bc44b2ad60a26df87b9eb7c745113dba051895
MirandaMG20 <guadamiran@gmail.com>
2023-10-13 12:15:24 -0400

    FAQ

---
+++ frontend/src/components/Faq/FaqItem.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const FagItem = ({ item }) => {
+  return (
+    <div className='p-3 lg:p-5 rounded-[12px] border border-solid border-[#d9dce2] mb-5 cursor-pointer'>
+      <div className='flex items-center justify-between gap-5'>
+        <h4 className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor">
+          {item.question}
+        </h4>
+      </div>
+    </div>
+  )
+}
+
+export default FagItem
