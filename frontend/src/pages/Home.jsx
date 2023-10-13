60153489e884d14a10a0011d089a80dbcb642cfd
MirandaMG20 <guadamiran@gmail.com>
2023-10-13 12:58:49 -0400

    FAQ

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -270,6 +270,18 @@ const Home = () => {
       </section >
       {/* FAQ SECTION END*/}
 
+      {/* TESTIMONIAL */}
+      <section>
+        <div className='container'>
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">What our patient say</h2>
+            <p className="text__para text-center">
+              Wold-class care for everyone. Our health system offers unmatched expert health care.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* TESTIMONIAL END*/}
     </>
   )
 }
