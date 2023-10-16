1fdb9c008260d7da6366936053d57092ea005440
MirandaMG20 <guadamiran@gmail.com>
2023-10-16 15:39:32 -0400

    Feedback

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -4,6 +4,7 @@ import { formatDate } from '../../utils/formatDate'
 const DoctorAbout = () => {
     return (
         <div>
+
             <div>
                 <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
                     About of
@@ -26,7 +27,7 @@ const DoctorAbout = () => {
                     <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
                         <div>
                             <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                                23 Jun, 2008
+                                {formatDate('08-23-2014')} - {formatDate('06-13-2016')}
                             </span>
                             <p className="text-[16px] leading-6 font-medium text-textColor">
                                 PHD in Surgeon
@@ -40,10 +41,10 @@ const DoctorAbout = () => {
                     <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
                         <div>
                             <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                            {formatDate('12-04-2010')}
+                                {formatDate('09-04-2010')} - {formatDate('06-13-2014')}
                             </span>
                             <p className="text-[16px] leading-6 font-medium text-textColor">
-                                PHD in Surgeon
+                                Surgeon
                             </p>
                         </div>
                         <p className="text-[14px] leading-5 font-medium text-textColor">
@@ -51,6 +52,39 @@ const DoctorAbout = () => {
                         </p>
                     </li>
 
+                </ul>
+            </div>
+
+            <div className="mt-12">
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
+                    Experience
+                </h3>
+
+                <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+                    <li className="p-4 rounded bg-[#fff9ea]">
+                        <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+                            {formatDate('07-04-2010')} - {formatDate('08-13-2014')}
+                        </span>
+                        <p className="text-[16px] leading-6 font-medium text-textColor">
+                            Sr. Surgeon
+                        </p>
+
+                        <p className="text-[14px] leading-5 font-medium text-textColor">
+                            Apollo Hospital, New York
+                        </p>
+                    </li>
+                    <li className="p-4 rounded bg-[#fff9ea]">
+                        <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+                            {formatDate('07-04-2010')} - {formatDate('08-13-2014')}
+                        </span>
+                        <p className="text-[16px] leading-6 font-medium text-textColor">
+                            Sr. Surgeon
+                        </p>
+
+                        <p className="text-[14px] leading-5 font-medium text-textColor">
+                            Apollo Hospital, New York
+                        </p>
+                    </li>
                 </ul>
             </div>
         </div>
