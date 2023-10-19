370f2437ce88e2872be8fe5c672c29fa1b4e10b7
MirandaMG20 <guadamiran@gmail.com>
2023-10-19 12:54:22 -0400

    Auth Controller

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,44 @@ import cookieParser from "cookie-parser";
 import cors from 'cors'
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+import authRoute from './Routes/auth.js'
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 8000
 
+app.listen(port, () => {
+    connectDB()
+    console.log(`Server is running on port localhost:${port}`)
+})
+
 const corsOptions = {
-    origin:true
+    origin: true
 }
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('API is working!')
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port localhost:${port}`)
-})
+//DATABASE CONNECTIONS
+mongoose.set('strictQuery', false)
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        console.log('MongoDB is connected')
+
+    } catch (err) {
+        console.log('MongoDB connection failed')
+    }
+}
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+app.use('/api/v1/auth', authRoute) // domain/api/v1/auth/register
