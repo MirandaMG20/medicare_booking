745c974c1343cedb8ddb7dca4c3a92c436c2e95d
MirandaMG20 <guadamiran@gmail.com>
2024-01-31 16:17:04 -0500

    Doc Profile

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
 
   const { data, loading, error } = useGetProfile(`${BASE_URL}/doctors/profile/me`)
 
-  const [tab,setTab]=useState('overview')
+  const [tab, setTab] = useState('overview')
 
   return (
     <section>
@@ -19,7 +19,32 @@ const Dashboard = () => {
 
         {!loading && !error && (
           <div className='grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]'>
-              <Tabs tab={tab} setTab={setTab} />
+            <Tabs tab={tab} setTab={setTab} />
+            <div className='lg:col-span-2'>
+              {data.isApproved === 'pending' && (
+                <div className='flex p-4 mb-4 text-yellow-800 bg-yellow-50 rounded-lg'>
+                  <svg
+                    aria-hidden='true'
+                    className='flex-shrink-0 w-5 h-5'
+                    fill='currentColor'
+                    viewBox='0 0 20 20'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
+                      clipRule='evenodd'
+                    ></path>
+                  </svg>
+
+                  <span></span>
+                  <div>
+                    To get approval please complete your profile. 
+                    We'll review it and approve within 48 hours.
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         )}
       </div>
